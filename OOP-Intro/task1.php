bb317e4790d8c57e0836df26a2b7eae41497e498
simonaTsenova <simona.tsenova17@gmail.com>
2015-10-22 22:19:52 +0300

    Commit1
    new commit

--- OOP-Intro/task1.php
+++ OOP-Intro/task1.php
@@ -1,32 +1,29 @@
 <?php
-	
 	class Student {
 		private $name;
 		private $grade;
 		private $gpa;
 		private $motivation;
-
 		public function __construct ($n = 'Unknown Student', $g = 'First', $avg = 6.00, $m = 0) {
-			echo "__construct for $n <br/> <br/>";
+			echo "__construct for $n <br/>";
 			$this->name = $n;
 			$this->grade = $g;
 			$this->gpa = $avg;
 			$this->motivation = $m;
 		}  
-
 		public function show_info () {
+			echo "<br/>";
 			echo "Personl information: <br/>";
 			echo "Name: $this->name <br/>";
 			echo "Grade: $this->grade <br/>";
 			echo "GPA: $this->gpa <br/>";
 			echo "Motivation Coefficient: $this->motivation <br/>";
+			echo "<br/>";
 		}
-
 		public function go_to_school () {
 			echo "$this->name is going to school at 7.30 a.m. School finishes at 1.30 p.m. <br/>";
 			$this->motivation = 1;
 		}
-
 		private function study ($hours) {
 			return $hours;
 			$tiredness = 20 - $hours;
@@ -38,7 +35,6 @@
 			}
 			$this->motivation = 0;
 		}
-
 		private function do_homework () {
 			if ($this->motivation == 1) {
 				echo "$this->name is doing homework. <br/>";
@@ -46,17 +42,17 @@
 				echo "$this->name can't do any homework now. Too tired. <br/>";
 			}
 		}
-
 		public function do_test ($subject, $grade) {
 			echo "$this->name is having a test on $subject and got $grade grade. <br/>";
 		}
 }
-
 		$kate = new Student('Kate');
 		$kate->show_info();
 		$kate->go_to_school();
 		$kate->do_test('maths', 5.9);
+		$kate->show_info();
 		$john = new Student('John', 'third', 5.55);
 		$john->show_info();
+		$john->go_to_school();
 	
 ?>
